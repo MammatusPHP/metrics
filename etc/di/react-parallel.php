4ef5ad942be0459370dbeac46fcfd5bbcfc1abc2
Cees-Jan Kiewiet <git@wyrihaximus.net>
2020-11-21 14:07:00 +0100

    Use the first batch of stable packages

--- etc/di/react-parallel.php
+++ etc/di/react-parallel.php
@@ -2,10 +2,11 @@
 
 use React\EventLoop\LoopInterface;
 use ReactParallel\Factory;
+use ReactParallel\Metrics;
 use WyriHaximus\Metrics\Registry;
 
 return [
     Factory::class => static function (LoopInterface $loop, Registry $registry) {
-        return (new Factory($loop))->withMetrics($registry);
+        return (new Factory($loop))->withMetrics(Metrics::create($registry));
     },
 ];
