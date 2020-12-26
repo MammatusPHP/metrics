fc4fc51413524d5ec025c7f226a11d7d2b2039a8
Cees-Jan Kiewiet <git@wyrihaximus.net>
2020-12-26 16:44:09 +0100

    Don't create an infinite loop between threads

--- etc/di/object-proxy.php
+++ etc/di/object-proxy.php
@@ -3,13 +3,15 @@
 use ReactParallel\Factory;
 use ReactParallel\ObjectProxy\Configuration;
 use ReactParallel\ObjectProxy\Proxy;
+use WyriHaximus\Metrics\Configuration as MetricsConfiguration;
 use WyriHaximus\Metrics\LazyRegistry\Registry as LazyRegistry;
+use WyriHaximus\Metrics\InMemory\Registry as InMemoryRegistry;
 use WyriHaximus\Metrics\Registry;
 
 return [
     Proxy::class => static function (Factory $factory, LazyRegistry $registry) {
         $proxy = (new Proxy((new Configuration($factory))->withMetrics(Configuration\Metrics::create($registry))));
-        $registry->register($proxy->thread($registry, Registry::class));
+        $registry->register($proxy->thread(new InMemoryRegistry(MetricsConfiguration::create()), Registry::class));
 
         return $proxy;
     },
