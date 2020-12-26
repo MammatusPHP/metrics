f55e20dec540072796d2081cb7e58a277ee304bc
Cees-Jan Kiewiet <git@wyrihaximus.net>
2020-12-26 15:44:25 +0100

    Use lazy registry to put metrics handling inside a thread

--- etc/di/object-proxy.php
+++ etc/di/object-proxy.php
@@ -3,12 +3,12 @@
 use ReactParallel\Factory;
 use ReactParallel\ObjectProxy\Configuration;
 use ReactParallel\ObjectProxy\Proxy;
-use WyriHaximus\Metrics\Registry;
+use WyriHaximus\Metrics\LazyRegistry\Registry;
 
 return [
     Proxy::class => static function (Factory $factory, Registry $registry) {
         $proxy = (new Proxy((new Configuration($factory))->withMetrics(Configuration\Metrics::create($registry))));
-        $proxy->create($registry, Registry::class, true);
+        $registry->register($proxy->thread($registry, Registry::class));
 
         return $proxy;
     },
