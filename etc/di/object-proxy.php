99194c1f5985a08577f8e756dbd11c87246d845c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-01-19 23:48:59 +0100

    Revert "Revert metrics in a thread"

--- etc/di/object-proxy.php
+++ etc/di/object-proxy.php
@@ -9,13 +9,17 @@ use ReactParallel\ObjectProxy\ProxyListInterface;
 use WyriHaximus\Metrics\Registry;
 use WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger;
 
+use WyriHaximus\Metrics\Configuration as MetricsConfiguration;
+use WyriHaximus\Metrics\LazyRegistry\Registry as LazyRegistry;
+use WyriHaximus\Metrics\InMemory\Registry as InMemoryRegistry;
+
 return [
     ProxyListInterface::class => static fn (): ProxyListInterface => new ProxyList(),
-    Configuration::class => static fn (Factory $factory, Registry $registry, ProxyListInterface $proxyList): Configuration => (new Configuration($factory))->withProxyList($proxyList)->withMetrics(Configuration\Metrics::create($registry)),
-    Proxy::class => static function (Configuration $configuration, Registry $registry, LoggerInterface $logger) {
+    Configuration::class => static fn (Factory $factory, LazyRegistry $registry, ProxyListInterface $proxyList): Configuration => (new Configuration($factory))->withProxyList($proxyList)->withMetrics(Configuration\Metrics::create($registry)),
+    Proxy::class => static function (Configuration $configuration, LazyRegistry $registry, LoggerInterface $logger) {
         $proxy = new Proxy($configuration);
         $proxy->on('error', CallableThrowableLogger::create($logger));
-        $proxy->share($registry, Registry::class);
+        $registry->register($proxy->thread(new InMemoryRegistry(MetricsConfiguration::create()), Registry::class));
 
         return $proxy;
     },
