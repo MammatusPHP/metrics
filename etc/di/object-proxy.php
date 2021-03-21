8e6d3e55d068a537641a17df8266561e75a913c7
Cees-Jan Kiewiet <git@wyrihaximus.net>
2021-03-21 13:44:52 +0100

    Log errors from object proxy

--- etc/di/object-proxy.php
+++ etc/di/object-proxy.php
@@ -1,17 +1,20 @@
 <?php declare(strict_types=1);
 
+use Psr\Log\LoggerInterface;
 use ReactParallel\Factory;
 use ReactParallel\ObjectProxy\Configuration;
 use ReactParallel\ObjectProxy\Generated\ProxyList;
 use ReactParallel\ObjectProxy\Proxy;
 use ReactParallel\ObjectProxy\ProxyListInterface;
 use WyriHaximus\Metrics\Registry;
+use WyriHaximus\PSR3\CallableThrowableLogger\CallableThrowableLogger;
 
 return [
     ProxyListInterface::class => static fn (): ProxyListInterface => new ProxyList(),
     Configuration::class => static fn (Factory $factory, Registry $registry, ProxyListInterface $proxyList): Configuration => (new Configuration($factory))->withProxyList($proxyList)->withMetrics(Configuration\Metrics::create($registry)),
-    Proxy::class => static function (Configuration $configuration, Registry $registry) {
+    Proxy::class => static function (Configuration $configuration, Registry $registry, LoggerInterface $logger) {
         $proxy = new Proxy($configuration);
+        $proxy->on('error', CallableThrowableLogger::create($logger));
         $proxy->share($registry, Registry::class);
 
         return $proxy;
